5007eef7a2af169e529325fc1ae898d2a39fe695
aethe <andrew.aethe@gmail.com>
2015-08-27 11:37:18 +0200

    Initial Commit

---
+++ Example/Pods/Target Support Files/ZenLayout/ZenLayout-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZenLayout : NSObject
+@end
+@implementation PodsDummy_ZenLayout
+@end
---
+++ Example/Pods/Target Support Files/ZenLayout/ZenLayout-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double ZenLayoutVersionNumber;
+FOUNDATION_EXPORT const unsigned char ZenLayoutVersionString[];
+
